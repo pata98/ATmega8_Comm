a080618f6f062a80d5b0ed1b21bd818b4b3b9bca
pata98 <yoopata@postech.ac.kr>
2021-10-26 02:10:20 +0900

    initial commit

---
+++ M8_UART/M8_UART/pata_UART.c
@@ -0,0 +1,82 @@
+﻿/*
+ * M8_UART.c
+ *
+ * Created: 2021-12-26
+ *  Author: Pata
+ */ 
+#include <avr/io.h>
+#include "pata_UART.h"
+#include <stdint.h>
+
+volatile uint8_t data = 0;
+volatile uint8_t data_m[10] = {0};
+uint8_t ubrr = 0;
+
+
+void UART_init(uint8_t baud, uint8_t mode)
+{
+	switch (baud)
+	{
+		case 1:
+			ubrr = 51;
+			break;
+		case 2:
+			ubrr = 34;
+			break;
+		case 3:
+			ubrr = 25;
+			break;
+		case 4:
+			ubrr = 12;
+			break;
+	}
+	UBRRH = (ubrr >> 8);
+	UBRRL = ubrr;
+	
+	if (mode & TX)
+		UCSRB = (1 << TXEN);
+	if ((mode & RX) || (mode & RXI))
+		UCSRB |= (1 << RXEN);
+	if (mode & RXI)
+		UCSRB |= (1 << RXCIE);
+	
+	DDRD &= ~(1<<0);
+	DDRD |= (1<<1);
+	
+}
+
+void UART_tx(uint8_t data)
+{
+	while(!(UCSRA & (1<<UDRE)));
+	UDR = data;
+	
+}
+
+void UART_tx_m(uint8_t *data, uint8_t length)
+{
+	for (uint8_t i = 0; i < length; i++)
+	{
+		while(!(UCSRA & (1<<UDRE)));
+		UDR = data[i];
+	}
+	
+}
+
+uint8_t UART_rx()
+{
+	while (!(UCSRA & (1<<RXC)));
+	data = UDR;
+	
+	return data;
+	
+}
+
+void UART_rx_m(uint8_t *data, uint8_t length)
+{
+	for (uint8_t i = 0; i < length; i++)
+	{
+		while(!(UCSRA & (1<<RXC)));
+		UDR = data[i];
+	}
+	
+}
---
+++ M8_UART/M8_UART/pata_UART.h
@@ -0,0 +1,58 @@
+﻿/*
+ * M8_UART.h
+ *
+ * Created: 2021-1-26
+ *  Author: Pata
+ *
+ *
+ *
+ *						Atmega8 pin map
+ *
+ *				 RESET  = PC6   PC5 =
+ *					RX	= PD0   PC4 =
+ *					TX	= PD1   PC3 =
+ *						= PD2   PC2 =
+ *						= PD3   PC1 =
+ *						= PD4   PC0 =
+ *				 VCC	= VCC   GND =
+ *				 GND	= GND   AREF=
+ *						= PB6   AVCC=
+ *            			= PB7   PB5 =
+ *			  			= PD5   PB4 =
+ *						= PD6   PB3 =
+ *						= PD7   PB2 =
+ *						= PB0   PB1 =
+ *
+ */ 
+
+#ifndef PATA_UART_H_
+#define PATA_UART_H_
+#ifndef F_CPU
+#define F_CPU 8000000UL
+#endif
+
+#define TX		0x01
+#define RX		0x02
+#define TXRX	0x03
+#define RXI		0x04
+#define TXRXI	0x05
+
+#define BAUD_9600	1
+#define BAUD_14k	2
+#define BAUD_19k	3
+#define BAUD_38k	4
+
+
+// UART initialize
+void UART_init(uint8_t baud, uint8_t mode);
+
+// Standard TX/RX
+void UART_tx(uint8_t data);
+void UART_tx_m(uint8_t *data, uint8_t length);
+uint8_t UART_rx();
+void UART_rx_m(uint8_t *data, uint8_t length);
+void UART_NWL();
+
+
+
+#endif /* PATA_UART_H_ */
